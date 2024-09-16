bb934c16c22f35d61f39b79d90201dde0f652aa6
Reynerio G. Samos <Reynerio.Samos@gmail.com>
2024-09-16 14:53:31 -0400

    changes to healthchecker

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (a *applicationDependencies) healthCheckHandler(w http.ResponseWriter,
 	r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", a.config.environment)
-	fmt.Fprintf(w, "version: %s\n", appVersion)
+	data := map[string]string{
+		"status":      "available",
+		"environment": a.config.environment,
+		"version":     appVersion}
+	jsResponse, err := json.Marshal(data)
+	if err != nil {
+		a.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	jsResponse = append(jsResponse, '\n')
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsResponse)
 }
