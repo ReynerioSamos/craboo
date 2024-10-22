e0e2d7e1f947fea156c5837b53fbb1c8494c696e
Reynerio G. Samos <Reynerio.Samos@gmail.com>
2024-10-22 15:44:52 -0400

    updated helpers.go with string package to handle body Byte Size

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
 // This Method will accept:
@@ -37,11 +38,21 @@ func (a *applicationDependencies) writeJson(w http.ResponseWriter, status int, d
 }
 
 func (a *applicationDependencies) readJson(w http.ResponseWriter, r *http.Request, destination any) error {
-	err := json.NewDecoder(r.Body).Decode(destination)
+	// what is the max size of the request body (250KB seems reasonable)
+	maxBytes := 256_000
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	// our decoder will check for unknown fields
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	//let start the decoding
+
+	//err := json.NewDecoder(r.Body).Decode(destination)
+	err := dec.Decode(destination) //OCT 22,2024
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
 		var invalidUnmarshalError *json.InvalidUnmarshalError
+		var maxBytesError *http.MaxBytesError
 
 		switch {
 		case errors.As(err, &syntaxError):
@@ -58,6 +69,15 @@ func (a *applicationDependencies) readJson(w http.ResponseWriter, r *http.Reques
 
 		case errors.Is(err, io.EOF):
 			return errors.New("the body must not be empty")
+
+		// check for unknown field error
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return fmt.Errorf("body contain unknown key %s", fieldName)
+
+		// does the body exceed our limit of 250KB?
+		case errors.As(err, &maxBytesError):
+			return fmt.Errorf("the body must not be larger than %d bytes", maxBytesError.Limit)
 		// the programmer messed up
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
@@ -65,6 +85,14 @@ func (a *applicationDependencies) readJson(w http.ResponseWriter, r *http.Reques
 		default:
 			return err
 		}
+	}
+	// Check if there is any data after the valid JSON data.
+	// Person might be trying to send multiple bodies during one request
+	// call decode once more to see if it gives us back anything
+	// we use a throw away struct 'struct{}{}' to hold result
+	err = dec.Decode(&struct{}{})
+	if !errors.Is(err, io.EOF) {
+		return errors.New("the body must only contain a single JSON value")
 	}
 	return nil
 }
