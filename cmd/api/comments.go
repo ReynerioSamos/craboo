306c31738a51e17dd3f0e1e4ef9568f4e6f18bed
Reynerio G. Samos <Reynerio.Samos@gmail.com>
2024-10-25 20:35:42 -0400

    added validators for Content and Author for comments

--- cmd/api/comments.go
+++ cmd/api/comments.go
@@ -7,7 +7,8 @@ import (
 
 	// import the data package which contains the definition for Comment
 	// _[space] is used as placeholder to let compiler know to ignore this dependancy
-	_ "github.com/ReynerioSamos/craboo/internal/data"
+	"github.com/ReynerioSamos/craboo/internal/data"
+	"github.com/ReynerioSamos/craboo/internal/validator"
 )
 
 func (a *applicationDependencies) createCommentHandler(w http.ResponseWriter, r *http.Request) {
@@ -24,6 +25,23 @@ func (a *applicationDependencies) createCommentHandler(w http.ResponseWriter, r 
 		a.badRequestResponse(w, r, err)
 		return
 	}
+
+	// Copy the values from incomingData to a new Comment struct
+	// At this point in our code the JSON is well-formed JSON so now
+	// we will validate it using the Validators which expects a Comment
+	comment := &data.Comment{
+		Content: incomingData.Content,
+		Author:  incomingData.Author,
+	}
+	// Intialize Validator instance
+	v := validator.New()
+	// Do the validation
+	data.ValidateComment(v, comment)
+	if !v.IsEmpty() {
+		a.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
 	// for now display the result
 	fmt.Fprintf(w, "%+v\n", incomingData)
 }
